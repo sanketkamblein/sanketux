3bd62cc9041fcf8851e451ddd77f5c8460a25853
Kamble Sanket Sanjay <sanketsanjaykamble36@gmail.com>
2018-10-08 02:16:45 +0530

    art

--- js/aboutme.js
+++ js/aboutme.js
@@ -7,7 +7,14 @@ $(function() {
     let imgSrc = $(this).find("img").attr("src");
      $(this).css('background-image', 'url(' + imgSrc + ')');
   })
+    
+     $(".img-l").each(function() {
+    $(this).wrap("<div class='img-d'></div>")
+    let imgSrc = $(this).find("img").attr("src");
+     $(this).css('background-image', 'url(' + imgSrc + ')');
+  })
             
+          
   
 })
 
